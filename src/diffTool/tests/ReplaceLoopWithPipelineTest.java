480cdf77c8932a22d68f33824238df0eeaf82411
Pouryafard75 <pouryafard75@gmail.com>
2022-10-26 23:31:42 -0400

    URLs to test LoopAndPipeline

--- src/diffTool/tests/ReplaceLoopWithPipelineTest.java
+++ src/diffTool/tests/ReplaceLoopWithPipelineTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.refactoringminer.api.RefactoringMinerTimedOutException;
-
+import junit.runner.Version;
 import java.util.List;
 
 public class ReplaceLoopWithPipelineTest {
@@ -18,19 +18,26 @@ public class ReplaceLoopWithPipelineTest {
     {
         urls = new String[]{
                 "https://github.com/checkstyle/checkstyle/commit/de022d2434e58dd633fd50a7f9bb50565a8767b5", //0
-                "https://github.com/junit-team/junit5/commit/6b575f2ee5f02288a774ff0a85ce3a3e3cb6946f" //1
+                "https://github.com/junit-team/junit5/commit/6b575f2ee5f02288a774ff0a85ce3a3e3cb6946f", //1
+                "https://github.com/pouryafard75/TestCases/commit/7ad7095e47abc37587342bbe873bd1f177b87acb", // 2
+                "https://github.com/pouryafard75/TestCases/commit/a7288d93ff44f045a97568af62135b8ca7a5d0e3" // 3
         };
     }
+
     @Test
     public void test1() throws RefactoringMinerTimedOutException {
-        String url = urls[1];
+        String url = urls[0];
         ProjectASTDiff diff = ProjectASTDiffer.fromURL(url).diff();
         new WebDiff(diff).run();
+        System.out.println("");
         while(true) { }
     }
     @Test
-    public void test2()
-    {
-        System.out.println("test2");
+    public void test2() throws RefactoringMinerTimedOutException {
+        String url = "https://github.com/pouryafard75/TestCases/commit/9bfec59746de9bf6bc783aab5c68aa5ab8bbdd6d";
+        ProjectASTDiff diff = ProjectASTDiffer.fromURL(url).diff();
+        new WebDiff(diff).run();
+        System.out.println("");
+        while(true) { }
     }
 }
